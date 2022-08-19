c3333e7e0f9296ab1f31c0023302109d4530bec8
Samuel Escobar <samueb8@gmail.com>
2022-08-19 12:22:49 -0400

    proyecto finalizado

---
+++ src/com/company/Main.java
@@ -0,0 +1,73 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner teclado = new Scanner(System.in);
+    public static void main(String[] args) {
+        //Creacion de productos
+	    Producto A = new Producto(270);
+        Producto B = new Producto(340);
+        Producto C = new Producto(390);
+
+        while(true){
+            //Variables
+            int montoIngresado = 0;
+            int vuelto;
+            Producto productoEscogido = null;
+
+            //Ejecucion del programa
+            while(productoEscogido == null){
+                System.out.println("Elija producto: A, B, C");
+                String tipo = teclado.nextLine();
+                switch (tipo){
+
+                    case "A":
+                        productoEscogido = A;
+                        break;
+                    case "B":
+                        productoEscogido = B;
+                        break;
+                    case "C":
+                        productoEscogido = C;
+                        break;
+                }
+            }
+            montoIngresado = pedirDinero(productoEscogido);
+            vuelto = montoIngresado - productoEscogido.getPrecio();
+            darVuelto(vuelto);
+        }
+    }
+
+    private static int pedirDinero(Producto p){
+        int montoIngresado = 0;
+        System.out.println("Ingrese monedas: 10, 50, 100");
+            while(montoIngresado < p.getPrecio()){
+                int moneda = teclado.nextInt();
+                teclado.nextLine();
+                if(!(moneda == 10 || moneda == 50 || moneda == 100)){
+                    System.out.println("Error, la maquina no acepta esta moneda");
+                }else{
+                    montoIngresado += moneda;
+                }
+            }
+        return montoIngresado;
+    }
+
+    private static void darVuelto(int vuelto){
+        int deuda = vuelto;
+        System.out.println("Su vuelto:");
+        while(deuda > 0){
+            if(deuda >= 100){
+                System.out.println("100");
+                deuda -= 100;
+            }else if(deuda >= 50){
+                System.out.println("50");
+                deuda -= 50;
+            }else if(deuda >= 10){
+                System.out.println("10");
+                deuda -= 10;
+            }
+        }
+    }
+}
---
+++ src/com/company/Producto.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Producto {
+    int precio;
+    public Producto(int precio){
+        this.precio = precio;
+    }
+    public int getPrecio(){
+        return precio;
+    }
+}
